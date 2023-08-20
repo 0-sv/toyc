cb2cb16f6bd71bca3097904833ff567f1c04aa37
Sebastiaan Verbeek <ajsverbeek@gmail.com>
2023-08-20 16:50:56 +0200

    feat: lexer boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::{env, fs};
+
+#[derive(Debug)]
+enum TokenType {
+    OpenBrace,
+    CloseBrace
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let file_contents = fs::read_to_string(&args[1]).expect("Unable to read file");
+
+    let mut tokens = vec![];
+    for c in file_contents.chars() {
+        match c {
+            '{' => tokens.push(TokenType::OpenBrace),
+            '}' => tokens.push(TokenType::CloseBrace),
+            _ => {}
+        }
+    }
+
+    println!("{:?}", tokens);
 }
